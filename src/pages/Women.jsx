ea39fe7fe111329f60e4d8c3c476250cbb59af88
hamnamurtaza66 <hamnamurtaza66@gmail.com>
2025-04-27 12:14:05 +0500

    Update Women.jsx

--- src/pages/Women.jsx
+++ src/pages/Women.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { Link } from "react-router-dom"; 
-import "./women.css";
+import "./Women.css";
 import products from "../data/productDetail";
 
 const Women = () => {
